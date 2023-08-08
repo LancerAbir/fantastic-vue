53a477ebd3befcb40500766f5f0453b798964f57
LancerAbir <anjan.o24@gmail.com>
2023-08-08 17:55:59 +0600

    some change

--- src/components/TestOne.vue
+++ src/components/TestOne.vue
@@ -3,20 +3,32 @@ import { onMounted, ref } from "vue";
 
 const name = ref("Paklu");
 
+let hori = "Hare Krishna";
+let numberOfId = 2;
+let h2 = "text-4xl";
 const great = () => {
     console.log(`Hello ${name.value}`);
 };
 
+const dynamicClassAndId = {
+    class: "text-8xl",
+    id: "13",
+};
+
+// onMounted Event Listeners এটি সব কাজ সেস করে সবার শেষে output দিবে
 onMounted(great);
 </script>
 
 <template>
     <div>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ab
-            accusamus, beatae fugit ipsa perspiciatis, recusandae reprehenderit
-            tenetur inventore velit possimus pariatur sit nulla esse magnam
-            expedita voluptate facere et?
+            {{ hori }}<br />
+            Lorem ipsum dolor sit am
         </p>
     </div>
+    <div v-html="hori"></div>
+    <input type="text" :id="numberOfId" />
+    <p :class="h2">text</p>
+
+    <h3 :="dynamicClassAndId">Dynamic Class And ID</h3>
 </template>
